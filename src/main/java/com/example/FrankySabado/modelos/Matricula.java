ce863d491f4d5189b72b323f5d8779d053e94e3d
Valdes-720 <sebastianvaldes910@gmail.com>
2025-10-25 14:02:22 -0500

    matricula y promedio corregidos

--- src/main/java/com/example/FrankySabado/modelos/Matricula.java
+++ src/main/java/com/example/FrankySabado/modelos/Matricula.java
@@ -1,72 +1,71 @@
 package com.example.FrankySabado.modelos;
 
 import jakarta.persistence.*;
-
+@Entity
+@Table(name = "matriculas", uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"estudiante_id", "materia_id", "grupo_id", "periodo"})
+})
 public class Matricula {
-	@Entity
-	@Table(name = "matricula", uniqueConstraints = {
-			@UniqueConstraint(columnNames = {"estudiante_id", "materia_id", "grupo_id", "periodo"})
-	})
-	public class Matricula {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-		@ManyToOne(optional = false)
-		@JoinColumn(name = "estudiante_id", referencedColumnName = "id")
-		private Estudiante estudiante;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "estudiante_id", referencedColumnName = "id")
+	private Estudiante estudiante;
 
-		@ManyToOne(optional = false)
-		@JoinColumn(name = "materia_id", referencedColumnName = "id")
-		private Materia materia;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "materia_id", referencedColumnName = "id")
+	private Materia materia;
 
-		@ManyToOne(optional = false)
-		@JoinColumn(name = "grupo_id", referencedColumnName = "id")
-		private Grupo grupo;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "grupo_id", referencedColumnName = "id")
+	private Grupo grupo;
 
-		@Column(nullable = false)
-		private String periodo;
+	@Column(nullable = false)
+	private String periodo;
 
-		public Long getId() {
-			return id;
-		}
-
-		public Estudiante getEstudiante() {
-			return estudiante;
-		}
-
-		public Materia getMateria() {
-			return materia;
-		}
-
-		public Grupo getGrupo() {
-			return grupo;
-		}
-
-		public String getPeriodo() {
-			return periodo;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public void setEstudiante(Estudiante estudiante) {
-			this.estudiante = estudiante;
-		}
-
-		public void setMateria(Materia materia) {
-			this.materia = materia;
-		}
-
-		public void setGrupo(Grupo grupo) {
-			this.grupo = grupo;
-		}
-
-		public void setPeriodo(String periodo) {
-			this.periodo = periodo;
-		}
+	public Long getId() {
+		return id;
 	}
 
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+
+	public Materia getMateria() {
+		return materia;
+	}
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public String getPeriodo() {
+		return periodo;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
+
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
 }
+
+
+
